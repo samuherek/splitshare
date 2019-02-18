f31380dff7b44f720d329165937d366de1a5fe0c
Sam Uherek <samuherekbiz@gmail.com>
2019-02-18 21:32:19 +0100

    Feat: connect web-native with common

--- packages/web-native/src/App.tsx
+++ packages/web-native/src/App.tsx
@@ -1,16 +1,3 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-
-const App: React.FC = () => {
-  const [counter, setCount] = React.useState(0);
-
-  return (
-    <View>
-      <Text>Hello world</Text>
-      <Text>{counter}</Text>
-      <Button title="increment" onPress={() => setCount(counter + 1)} />
-    </View>
-  );
-};
+import { App } from '@splitshare/common';
 
 export default App;
